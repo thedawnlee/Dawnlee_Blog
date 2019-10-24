3fb70514526b02d4ab01e17a3dcbfa439127fa96
lisantao <lisantao_tao@outlook.com>
2019-10-24 12:51:20 +0800

    init repo

---
+++ src/main/java/com/dawnlee/bolg/DawnleeApplication.java
@@ -0,0 +1,13 @@
+package com.dawnlee.bolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawnleeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DawnleeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dawnlee/bolg/DawnleeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawnlee.bolg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawnleeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
